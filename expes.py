e5130fbf4b1420bcbda21916cc2fd56fc4520193
Lucas Langlade <lucaslanglade24@gmail.com>
2025-08-04 12:54:51 -0400

    pre expes

---
+++ expes.py
@@ -0,0 +1,51 @@
+import subprocess 
+import os
+import argparse
+ 
+if __name__ == "__main__":
+    chemin_dossier = "dsirp/standard/"
+    listInstance = []
+    for nbClients in [5,10,15,25,50,75,100,150,200]:
+        for p in [5,10,20]:
+            for i in [1,2,3,4,5]:
+                listInstance.append("dirp-" + str(nbClients) + "-" + str(p) + "-" + str(i) + ".dat")
+
+    launchAll = False
+
+    if launchAll:
+        average = 0
+        errorList = []
+        for nom_element in listInstance:
+            chemin_element = os.path.join(chemin_dossier, nom_element)
+            if os.path.isfile(chemin_element):
+                print(chemin_element)
+                result = subprocess.Popen(['./irp.exe', chemin_element, '-veh', '1',  '-seed',  '55',  '-scenarios', '1', '-iter', '10000', "-iterNonProd", '10000', '-time', '3', '-traces', '0'], stdout=subprocess.PIPE, text=True, stderr=subprocess.PIPE)
+                for res in result.stdout:
+                    average += float(res)
+                    print(res)
+                for err in result.stderr:
+                    errorList.append([chemin_element, err])
+                    print(err)
+        print(average/len(listInstance) )
+
+    else:
+        parser = argparse.ArgumentParser(description = 'Launch experiences')
+        parser.add_argument('--expe_id', type = int, default = 134)
+        args = parser.parse_args()
+
+        expe_id = args.expe_id
+        nbScenario = 10
+        nbVehicle = 1
+        seed = 42
+        maxIter = 20000
+        maxIterNonProd = 20000
+        maxTime = 600
+        chemin_element = os.path.join(chemin_dossier, listInstance[expe_id])
+
+        if os.path.isfile(chemin_element):
+            result = subprocess.Popen(['./irp.exe', chemin_element, '-veh', str(nbVehicle), '-seed', str(seed), '-scenarios', str(nbScenario), '-iter', str(maxIter), "-iterNonProd", str(maxIterNonProd), '-time', str(maxTime), '-traces', '0'], stdout=subprocess.PIPE, text=True, stderr=subprocess.PIPE)
+            for res in result.stdout:
+                # average += float(res)
+                print(res)
+            for err in result.stderr:
+                print(err)
---
+++ job.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#SBATCH --array=0-135
+#SBATCH --cpus-per-task=16
+#SBATCH --mem=36G
+#SBATCH --time=04:00:00
+#SBATCH --job-name=expes_DSIRP
+#SBATCH -o expes_out/expes_%A.out
+#SBATCH -e expes_out/expes_%A.errarray
+
+python3.10 ssystems/expes.py --expe_id=$SLURM_ARRAY_TASK_ID
