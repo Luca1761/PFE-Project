7031cc215caad016084b3fd003658e9610ec8f18
Lucas Langlade <lucaslanglade24@gmail.com>
2025-08-04 14:11:19 -0400

    modifs for job

--- job.sh
+++ job.sh
@@ -7,4 +7,4 @@
 #SBATCH -o expes_out/expes_%A.out
 #SBATCH -e expes_out/expes_%A.errarray
 
-python3.10 ssystems/expes.py --expe_id=$SLURM_ARRAY_TASK_ID
+python3.10 expes.py --expe_id=$SLURM_ARRAY_TASK_ID
